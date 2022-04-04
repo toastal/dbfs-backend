076b085d924a0b143aab69fb0091a1a75d240b3e
muralimariyappan <muralimariyappan13@gmail.com>
2022-04-04 20:06:46 +0530

    add mail configs

--- app/Notifications/ForgotPassword.php
+++ app/Notifications/ForgotPassword.php
@@ -47,6 +47,7 @@ class ForgotPassword extends Notification
         $url = url(config('app.spa_url').'/auth/reset/'.$this->token);
 
         return (new MailMessage)
+            ->subject('Reset Password Notification')
             ->line('You are receiving this email because we received a password reset request for your account.')
             ->action('Reset Password', $url)
             ->line('If you did not request a password reset, no further action is required.');
